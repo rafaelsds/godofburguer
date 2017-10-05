3bb95dc7e3d5c6274f35012ceb683b7470b3d2c2
Rafael <rafael.sds@live.com>
2017-10-05 20:24:05 -0300

    add add activty insumos

---
+++ app/src/main/java/com/godofburguer/app/godofburguer/entidades/Insumos.java
@@ -0,0 +1,22 @@
+package com.godofburguer.app.godofburguer.entidades;
+
+
+
+public class Insumos {
+
+    private String nome;
+
+
+    public Insumos(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
