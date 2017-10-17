cff062c2f5b309f6ac7529bee879484981275378
AlcinoBarros <steveneizy1294@gmail.com>
2017-10-17 14:12:47 -0200

    layout activity_listagem_fornecedores classe fornecedoresActivity layout activity_fornecedores

---
+++ app/src/main/java/com/godofburguer/app/godofburguer/FornecedoresActivity.java
@@ -0,0 +1,17 @@
+package com.godofburguer.app.godofburguer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by shepard363 on 17-Oct-17.
+ */
+
+public class FornecedoresActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fornecedores);
+    }
+}
